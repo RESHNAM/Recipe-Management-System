3b64b320cc5ef81e5bdab67878932943abea755d
Renney Namale <renney003@gmail.com>
2024-05-18 11:31:41 +0300

    recipe model plus migration

--- recipe/apprecipe/models.py
+++ recipe/apprecipe/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Recipe(models.Model):
+    CATEGORY_CHOICES = (
+        ('amateur', 'amateur'),
+        ('regular', 'regular'),
+        ('professional', 'professional')
+    )
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} : {}".format(self.title, self.description)
