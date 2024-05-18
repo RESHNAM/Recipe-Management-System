833bb3d8dfce10fa27f28ebd7fd32a9017ccc76e
Renney Namale <renney003@gmail.com>
2024-05-18 11:44:37 +0300

    include the model to admin dashboard plus updated migrations

--- recipe/apprecipe/admin.py
+++ recipe/apprecipe/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'description',
+        'ingredients',
+        'instructions',
+        'category',
+        'created_at',
+        'updated_at'
+    ]
+    list_filter = ['title', 'category', 'created_at']
+    search_fields = ['title', 'category', 'created_at']
+
+admin.site.register(Recipe, RecipeAdmin)
--- recipe/apprecipe/models.py
+++ recipe/apprecipe/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Recipe(models.Model):
@@ -8,7 +8,7 @@ class Recipe(models.Model):
         ('regular', 'regular'),
         ('professional', 'professional')
     )
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     ingredients = models.TextField()
